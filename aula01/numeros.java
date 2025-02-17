a9f66d2d960cbfabdac953b91f1a24b6cf6583d5
Gabriel <saygabrielfernandes3105@gmail.com>
2025-02-17 09:18:50 -0300

    Aula sobre estrutura e códigos básicos em Java

---
+++ aula01/numeros.java
@@ -0,0 +1,8 @@
+public class numeros{
+    public static void main(String[] args) {
+        System.out.println(3 + 4);
+        System.out.println(5 - 2);
+        System.out.println(3 * 4);
+        System.out.println(100 / 5);
+    }
+}
