d7b558f8485bdecc585ce5bde9d993f747e90d72
Gabriel Fernandes <166411483+Gabriel-Fern@users.noreply.github.com>
2025-06-23 00:03:02 -0300

    Add files via upload

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -10,16 +10,25 @@ package calculadora;
  */
 public class Calculadora {
 
+<<<<<<< HEAD
+=======
     private void Soma(java.awt.event.Action.Event){
     
+>>>>>>> ba4a5d0ab253d844b508a7bd9a638ae812cbf79e
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+<<<<<<< HEAD
+    }
+    
+}
+=======
         
         
     }
     
 }
 }
+>>>>>>> ba4a5d0ab253d844b508a7bd9a638ae812cbf79e
